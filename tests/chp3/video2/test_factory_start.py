cff7df4f2cc16172c31df51b08da9a8cd35f2724
Alberto Nicolás Mikulan <betomiku@gmail.com>
2022-03-05 19:04:38 -0300

    extracted test check to a fixture. Don't know if this is good or bad

--- tests/chp3/video2/test_factory_start.py
+++ tests/chp3/video2/test_factory_start.py
@@ -24,6 +24,25 @@ def process_data(city_list_location):
     yield _specify_type  # when called, we specify the filename or the type
 
 
+@pytest.fixture(scope="function")
+def check_processed_data():
+
+    def _specify_data(processed_data):
+        for row in processed_data:
+            assert(isinstance(row['Country'], str))
+            assert(isinstance(row['City'], str))
+            assert(isinstance(row['State_Or_Province'], str))
+            assert(isinstance(row['Lat'], float))
+            assert(isinstance(row['Long'], float))
+            assert(isinstance(row['Altitude'], float))
+
+        # Basic processed_data checks
+        assert len(processed_data) == 180  # We have collected 180 rows
+        assert processed_data[0]['Country'] == 'Andorra'
+        assert processed_data[106]['Country'] == 'Japan'
+    yield _specify_data
+
+
 def test_csv_reader_header_fields(process_data):
     """
     Happy Path test to make sure the processed data
@@ -41,26 +60,13 @@ def test_csv_reader_header_fields(process_data):
             ]
 
 
-def test_csv_reader_data_contents(process_data):
+def test_csv_reader_data_contents(process_data, check_processed_data):
     """
     Happy Path Test to examine that each row
     had the appropriate data type per field
     """
     data = process_data(file_name_or_type='clean_map.csv')
-
-    # Check row types
-    for row in data:
-        assert(isinstance(row['Country'], str))
-        assert(isinstance(row['City'], str))
-        assert(isinstance(row['State_Or_Province'], str))
-        assert(isinstance(row['Lat'], float))
-        assert(isinstance(row['Long'], float))
-        assert(isinstance(row['Altitude'], float))
-
-    # Basic data checks
-    assert len(data) == 180  # We have collected 180 rows
-    assert data[0]['Country'] == 'Andorra'
-    assert data[106]['Country'] == 'Japan'
+    check_processed_data(processed_data=data)
 
 
 def test_csv_reader_malformed_data_contents(process_data):
@@ -72,26 +78,13 @@ def test_csv_reader_malformed_data_contents(process_data):
     assert str(exp.value) == "could not convert string to float: 'not_an_altitude'"
 
 
-def test_json_reader_data_contents(process_data):
+def test_json_reader_data_contents(process_data, check_processed_data):
     """
     Happy Path Test to examine that each row
     had the appropriate data type per field
     """
     data = process_data(file_name_or_type='clean_map.json')
-
-    # Check row types
-    for row in data:
-        assert(isinstance(row['Country'], str))
-        assert(isinstance(row['City'], str))
-        assert(isinstance(row['State_Or_Province'], str))
-        assert(isinstance(row['Lat'], float))
-        assert(isinstance(row['Long'], float))
-        assert(isinstance(row['Altitude'], float))
-
-    # Basic data checks
-    assert len(data) == 180  # We have collected 180 rows
-    assert data[0]['Country'] == 'Andorra'
-    assert data[106]['Country'] == 'Japan'
+    check_processed_data(processed_data=data)
 
 
 def test_json_reader_malformed_data_contents(process_data):
