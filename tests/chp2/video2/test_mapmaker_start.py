ed8a243597477b79d8165b5febb3d38dedd323fc
Alberto Nicolás Mikulan <betomiku@gmail.com>
2022-03-05 12:32:10 -0300

    added test to check for invalid point generation (latitude <= -90)

--- tests/chp2/video2/test_mapmaker_start.py
+++ tests/chp2/video2/test_mapmaker_start.py
@@ -1,8 +1,13 @@
 from scripts.chp2.video2.mapmaker_start import Point
-
+import pytest
 
 def test_make_one_point():
     latitude = 50.4501
     longitude = 305234
     p1 = Point("Kiev", latitude, longitude)
     assert p1.latitudeAndLongitude() == (latitude, longitude)
+
+def test_invalid_point_generation():
+    with pytest.raises(Exception) as exp:
+        Point("Graz", -955.34, 36.444)
+
