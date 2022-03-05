f28e2f5f747af3059c41fc954007f46ad54ed82a
Alberto Nicolás Mikulan <betomiku@gmail.com>
2022-03-05 18:47:12 -0300

    refactored data processor

--- scripts/data_processor.py
+++ scripts/data_processor.py
@@ -1,28 +1,22 @@
 import csv
 import json
 
+def check_reader_data(data):
+    for row in data:
+            try:
+                row['Lat'] = float(row['Lat'])
+                row['Long'] = float(row['Long'])
+                row['Altitude'] = float(row['Altitude'])
+            except Exception as exp:
+                raise ValueError(str(exp))
+    return data
 
 def csv_reader(file_location):
     with open(file_location, mode='r') as csv_file:
         data = [line for line in csv.DictReader(csv_file)]
-        for row in data:
-            try:
-                row['Lat'] = float(row['Lat'])
-                row['Long'] = float(row['Long'])
-                row['Altitude'] = float(row['Altitude'])
-            except Exception as exp:
-                raise ValueError(str(exp))
-
-        return data
+        return check_reader_data(data)
 
 def json_reader(file_location):
     with open(file_location, mode='r') as json_file:
         data = json.loads(json_file.read())
-        for row in data:
-            try:
-                row['Lat'] = float(row['Lat'])
-                row['Long'] = float(row['Long'])
-                row['Altitude'] = float(row['Altitude'])
-            except Exception as exp:
-                raise ValueError(str(exp))
-        return data
+        return check_reader_data(data)
