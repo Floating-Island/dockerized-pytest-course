5219113bdfcdbf5bc69509ca47ef31fbd286e86c
Alberto Nicolás Mikulan <betomiku@gmail.com>
2022-03-05 18:21:24 -0300

    added tests to check for json reader

--- tests/chp3/video2/test_factory_start.py
+++ tests/chp3/video2/test_factory_start.py
@@ -70,3 +70,34 @@ def test_csv_reader_malformed_data_contents(process_data):
     with pytest.raises(ValueError) as exp:
         process_data(file_name_or_type='malformed_map.csv')
     assert str(exp.value) == "could not convert string to float: 'not_an_altitude'"
+
+
+def test_json_reader_data_contents(process_data):
+    """
+    Happy Path Test to examine that each row
+    had the appropriate data type per field
+    """
+    data = process_data(file_name_or_type='clean_map.json')
+
+    # Check row types
+    for row in data:
+        assert(isinstance(row['Country'], str))
+        assert(isinstance(row['City'], str))
+        assert(isinstance(row['State_Or_Province'], str))
+        assert(isinstance(row['Lat'], float))
+        assert(isinstance(row['Long'], float))
+        assert(isinstance(row['Altitude'], float))
+
+    # Basic data checks
+    assert len(data) == 180  # We have collected 180 rows
+    assert data[0]['Country'] == 'Andorra'
+    assert data[106]['Country'] == 'Japan'
+
+
+def test_json_reader_malformed_data_contents(process_data):
+    """
+    Sad Path Test
+    """
+    with pytest.raises(ValueError) as exp:
+        process_data(file_name_or_type='malformed_map.json')
+    assert str(exp.value) == "could not convert string to float: 'not_an_altitude'"
