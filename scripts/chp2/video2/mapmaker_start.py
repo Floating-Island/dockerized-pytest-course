2c61897a262f96df12bb35e3cec7fe1c5bece313
Alberto Nicolás Mikulan <betomiku@gmail.com>
2022-03-05 12:35:44 -0300

    addded exception when latitude < -90

--- scripts/chp2/video2/mapmaker_start.py
+++ scripts/chp2/video2/mapmaker_start.py
@@ -1,6 +1,8 @@
 class Point():
     def __init__(self, cityName, latitude, longitude):#__init__ denotes the class constructor
         self._cityName = cityName# _ indicates a private member variable
+        if (latitude < -90):
+            raise ValueError("Invalid latitude. Value must be greater or equal than -90")
         self._latitude = latitude
         self._longitude = longitude
     
