19899c8a09bac59140d623605a044cf92a02baa4
Alberto Nicolás Mikulan <betomiku@gmail.com>
2022-03-05 12:14:06 -0300

    fixed typo with latitudeAndLongitude call in make one point test

--- tests/chp2/video2/test_mapmaker_start.py
+++ tests/chp2/video2/test_mapmaker_start.py
@@ -4,4 +4,4 @@ def test_make_one_point():
     latitude = 50.4501
     longitude = 305234
     p1 = Point("Kiev", latitude, longitude)
-    assert p1.LatitudeAndLongitude() == (latitude, longitude)
+    assert p1.latitudeAndLongitude() == (latitude, longitude)
