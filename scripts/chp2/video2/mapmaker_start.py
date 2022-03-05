72343fdcecba3456771959a381e86311dbdbb864
Alberto Nicolás Mikulan <betomiku@gmail.com>
2022-03-05 12:11:02 -0300

    created __init__ method inside Point class

--- scripts/chp2/video2/mapmaker_start.py
+++ scripts/chp2/video2/mapmaker_start.py
@@ -1,2 +1,5 @@
 class Point():
-    pass
+    def __init__(self, cityName, latitude, longitude):#__init__ denotes the class constructor
+        self._cityName = cityName# _ indicates a private member variable
+        self._latitude = latitude
+        self._longitude = longitude
