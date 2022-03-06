b72912d6c5359b2e357a607b817d78769f534b64
Alberto Nicolás Mikulan <betomiku@gmail.com>
2022-03-06 15:58:39 -0300

    refactored test to be parametrized

--- tests/chp3/video4/test_param_challenge.py
+++ tests/chp3/video4/test_param_challenge.py
@@ -26,7 +26,12 @@ def process_data(city_list_location):
     yield _specify_type
 
 
-def test_csv_writer(process_data):
+@pytest.mark.parametrize("country,statistic,expectedValue", [
+    ('Andorra', 'Mean', 1641.42),
+    ('Andorra', 'Median', 1538.02),
+    ('Argentina', 'Median', 125.0)
+])
+def test_csv_writer(process_data, country, statistic, expectedValue):
     """
      TO DO: Update the function to be parametrized with 3 scenarios:
      ('Andorra', 'Mean', 1641.42),
@@ -41,9 +46,9 @@ def test_csv_writer(process_data):
       the csv writer.
     """
     data = process_data(file_name_or_type="clean_map.csv")
-    andorran_median_res = data_aggregator.altitude_stat_per_country(data, 'Andorra', 'Median')
+    country_statistic_res = data_aggregator.altitude_stat_per_country(data, country, statistic)
     output_location = StringIO()
-    data_aggregator.csv_writer(andorran_median_res, output_location)
+    data_aggregator.csv_writer(country_statistic_res, output_location)
 
     res = output_location.getvalue().strip('\r\n')
-    assert res == 'Country,Median\r\nAndorra,1538.02'
+    assert res == f"Country,{statistic}\r\n{country},{expectedValue}"
