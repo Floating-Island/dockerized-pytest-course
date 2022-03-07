234d074d27df3ff902ef2a279c06709f2711016e
Alberto Nicolás Mikulan <betomiku@gmail.com>
2022-03-07 10:31:02 -0300

    added parametrization to test

--- tests/chp3/video5/test_user_candy.py
+++ tests/chp3/video5/test_user_candy.py
@@ -3,8 +3,10 @@ import pytest
 from scripts.chp3.video5.user_candy import User
 
 
-def test_create_user():
-    height = 1.70
-    favoriteColor = 'red'
+@pytest.mark.parametrize("height, favoriteColor", [
+    (1.70, 'red'),
+    (2.90, 'yellow')
+])
+def test_create_user(height, favoriteColor):
     aUser = User(height, favoriteColor)
     assert aUser.height() == height and aUser.favoriteColor() == favoriteColor
