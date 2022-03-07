89a2cde3d4731ebc48bd68128f4d00ad372dbc2d
Alberto Nicolás Mikulan <betomiku@gmail.com>
2022-03-07 10:23:00 -0300

    modified conftest.py to show different ways of modularization

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,12 @@
+# a way of modularizing conftest which needs to disable pylint for unused imports:
+# from utility.cities import city_list_location #pylint:disable-unused-import
+# from utility.data_processing import process_data #pylint:disable-unused-import
+
 """
 Note:
 Fixtures with @pytest.fixture(scope="session", autouse=True) must remain in this file
 """
+# another way of modularizing conftest, specifying the pytest_plugins (I think it's better than the first one)
 pytest_plugins = [
    "tests.utility.cities",
    "tests.utility.data_processing",
