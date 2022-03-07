bca18405a83ba8b05a705650e127e760634605dd
Alberto Nicolás Mikulan <betomiku@gmail.com>
2022-03-07 10:36:40 -0300

    created test to give expected award based on user height

--- tests/chp3/video5/test_user_candy.py
+++ tests/chp3/video5/test_user_candy.py
@@ -1,6 +1,7 @@
 import pytest
 
 from scripts.chp3.video5.user_candy import User
+from scripts.chp3.video5.user_candy import Award
 
 
 @pytest.mark.parametrize("height, favoriteColor", [
@@ -10,3 +11,13 @@ from scripts.chp3.video5.user_candy import User
 def test_create_user(height, favoriteColor):
     aUser = User(height, favoriteColor)
     assert aUser.height() == height and aUser.favoriteColor() == favoriteColor
+
+
+@pytest.mark.parametrize("height, favoriteColor, expectedAward", [
+    (1.70, 'red', 'chocolate'),
+    (2.90, 'yellow', 'marshmallow')
+])
+def test_award_user(height, favoriteColor, expectedAward):
+    aUser = User(height, favoriteColor)
+    candy = Award.giveCandyTo(aUser)
+    assert candy == expectedAward
